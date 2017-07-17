75f1b833e7051f2808abb4cee6626ee0ca368be6
Edmund Mok <edmundmok@outlook.com>
2017-07-17 22:36:58 +0800

    Update new.sh to work with on Swift projects

--- new.sh
+++ new.sh
@@ -13,28 +13,47 @@ else
 fi
 
 temp_file=$(mktemp)
-BUILD_SETTINGS=`xcodebuild $FILE_TYPE $FILENAME_WITH_EXT -scheme $FILENAME -showBuildSettings > $temp_file`
-# cat $temp_file
+temp_dir=$(mktemp -d)
+BUILD_SETTINGS=`xcodebuild $FILE_TYPE $FILENAME_WITH_EXT -scheme $FILENAME -showBuildSettings -configuration Debug > $temp_file`
 
-SWIFT_FILES=$(find . -type f -name \*.swift | while read line; do echo -n "$line "; done)
+find . -name "*.swift" -not -path "*/Pods/*" -print0 | while read -d $'\0' file; do filename=${file##*/}; cp "$file" "$temp_dir/$file_name"; done
+SWIFT_FILES=$(find $temp_dir -type f -name \*.swift -not -path "*/Pods/*" | while read line; do echo -n "$line "; done)
+
 IPHONE_DEPLOYMENT_TARGET=$(grep 'IPHONEOS_DEPLOYMENT_TARGET = ' $temp_file | awk '{print $3}')
 SDKROOT=$(grep 'SDKROOT = ' $temp_file | awk '{print $3}')
 PLATFORM_DIR=$(grep 'PLATFORM_DIR = ' $temp_file | awk '{print $3}' | sort -r | head -n 1)
 BUILD_DIR=$(grep 'BUILD_DIR = ' $temp_file | awk '{print $3}' | head -n 1)
 FRAMEWORKS=""
 
-for framework in `grep 'OTHER_LDFLAGS' $temp_file | grep -o -- '-framework.*' | tr ' ' '\n' | grep -o '"[^"]\+"' | tr -d '"'`; do
-	FRAMEWORKS="$FRAMEWORKS -F $BUILD_DIR/Debug-iphoneos/$framework"
+for framework in `grep 'FRAMEWORK_SEARCH_PATHS' $temp_file | awk '{for(i=3;i<=NF;++i)print $i}'`; do
+	FRAMEWORKS="$FRAMEWORKS -F $framework"
+	# FRAMEWORKS="$FRAMEWORKS -F $BUILD_DIR/Debug-iphoneos/$framework"
 done
 
 FRAMEWORKS="$FRAMEWORKS -F $PLATFORM_DIR/Developer/Library/Frameworks"
-FRAMEWORKS="$FRAMEWORKS -I $BUILD_DIR/Debug-iphoneos/"
+FRAMEWORKS="$FRAMEWORKS -I $BUILD_DIR/Debug-iphoneos/ -F $BUILD_DIR/Debug-iphoneos"
+
+HEADERS=""
+
+for header in `grep 'HEADER_SEARCH_PATHS' $temp_file | sort -r | head -n 1 | awk '{for(i=3;i<=NF;++i)print $i}'`; do
+	HEADERS="$HEADERS -Xcc -I${header}"
+done
 
 # echo "SWIFT_FILES: $SWIFT_FILES"
 # echo "IPHONE_DEPLOYMENT_TARGET: $IPHONE_DEPLOYMENT_TARGET"
 # echo "SDKROOT: $SDKROOT"
 # echo "BUILD_DIR: $BUILD_DIR"
 # echo "FRAMEWORKS: $FRAMEWORKS"
+# echo "HEADERS: $HEADERS"
+
+/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift -frontend -c -suppress-warnings -num-threads 4 -primary-file $SWIFT_FILES -target armv7-apple-ios${IPHONE_DEPLOYMENT_TARGET} -enable-objc-interop -sdk $SDKROOT $FRAMEWORKS $HEADERS
+RET_VAL=$?
 
 rm ${temp_file}
-$(/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift -frontend -c -primary-file $SWIFT_FILES -target armv7-apple-ios${IPHONE_DEPLOYMENT_TARGET} -enable-objc-interop -sdk $SDKROOT $FRAMEWORKS)
+rm -rf ${temp_dir}
+
+get_return() {
+	return $1
+}
+
+get_return $RET_VAL
